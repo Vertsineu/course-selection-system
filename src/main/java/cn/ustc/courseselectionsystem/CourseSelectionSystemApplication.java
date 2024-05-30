aa8f03a12c927cfb49da07b4242bc0072010baf5
niuzai1000 <wuqingyu05@mail.ustc.edu.cn>
2024-05-30 20:07:18 +0800

    添加对于Spring Security的支持

--- src/main/java/cn/ustc/courseselectionsystem/CourseSelectionSystemApplication.java
+++ src/main/java/cn/ustc/courseselectionsystem/CourseSelectionSystemApplication.java
@@ -3,9 +3,11 @@ package cn.ustc.courseselectionsystem;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 @MapperScan(basePackages = "cn.ustc.courseselectionsystem.mapper")
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class CourseSelectionSystemApplication {
 
     public static void main(String[] args) {
