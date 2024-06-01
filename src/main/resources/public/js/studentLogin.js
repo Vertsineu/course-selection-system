d0718bd0f52ef8e54258802df0bf40cc0a6084fa
WAHearer <62363213+WAHearer@users.noreply.github.com>
2024-06-01 18:03:33 +0800

    Update studentLogin.js

--- src/main/resources/public/js/studentLogin.js
+++ src/main/resources/public/js/studentLogin.js
@@ -20,7 +20,7 @@ function login(){
 			var date=new Date();
 			date.setTime(date.getTime()+7*24*60*60*1000);
 			var expires="expires="+date.toUTCString;
-			document.cookie="token"+"="+result.data.token+";"+expires;
+			document.cookie="token"+"="+result.data.token+";"+expires+";path=/";
 			window.location.href="../studentMainpage.html";
 		}
 		else{
