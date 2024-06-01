102e911f664afce7293c2db50c9db115c1a92974
Qingyu Wu <wuqingyu05@mail.ustc.edu.cn>
2024-06-01 22:35:37 +0800

    删除去除Spring Security的代码

--- src/main/java/cn/ustc/courseselectionsystem/CourseSelectionSystemApplication.java
+++ src/main/java/cn/ustc/courseselectionsystem/CourseSelectionSystemApplication.java
@@ -3,12 +3,11 @@ package cn.ustc.courseselectionsystem;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 @MapperScan(basePackages = "cn.ustc.courseselectionsystem.mapper")
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 @EnableTransactionManagement
