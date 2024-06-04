66e5970d6842e2de2858e691565ceafc05a3887d
WAHearer <62363213+WAHearer@users.noreply.github.com>
2024-06-04 21:20:52 +0800

    Update studentMainpage.js

--- src/main/resources/public/js/studentMainpage.js
+++ src/main/resources/public/js/studentMainpage.js
@@ -148,9 +148,9 @@ function selectQuery(){
 		th1.style.width="30%";
 		th2.style.width="10%";
 		th3.style.width="10%";
-		th4.style.width="35%";
+		th4.style.width="30%";
 		th5.style.width="10%";
-		th6.style.width="5%";
+		th6.style.width="10%";
 		th1.textContent="课程";
 		th2.textContent="开课单位";
 		th3.textContent="授课教师";
@@ -279,9 +279,9 @@ function lastPageForSelect(){
 	th1.style.width="30%";
 	th2.style.width="10%";
 	th3.style.width="10%";
-	th4.style.width="35%";
+	th4.style.width="30%";
 	th5.style.width="10%";
-	th6.style.width="5%";
+	th6.style.width="10%";
 	th1.textContent="课程";
 	th2.textContent="开课单位";
 	th3.textContent="授课教师";
@@ -407,9 +407,9 @@ function nextPageForSelect(){
 	th1.style.width="30%";
 	th2.style.width="10%";
 	th3.style.width="10%";
-	th4.style.width="35%";
+	th4.style.width="30%";
 	th5.style.width="10%";
-	th6.style.width="5%";
+	th6.style.width="10%";
 	th1.textContent="课程";
 	th2.textContent="开课单位";
 	th3.textContent="授课教师";
